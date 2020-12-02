08de41dd492f20e9657b15986e70584229aa0e88
Treo <yuodaytis.dmitry@gmail.com>
2020-12-03 02:07:48 +0300

    full update project1

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-<use strict>
+"use strict";
+
+const number0fFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+let personalMovieDB = {
+    count: number0fFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+let a = prompt("Один из последних просмотренных фильмов?", "");
+personalMovieDB.movie[a] = prompt("На сколько оцените его?");
+
+let b = prompt("Второй из последних просмотренных фильмов?", "");
+personalMovieDB.movie[b] = prompt("На сколько оцените его?");
+
+console.log(personalMovieDB.count);
+console.log(personalMovieDB.movie);
+console.log(personalMovieDB.private);
+
